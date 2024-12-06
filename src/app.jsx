a9597d550dbc87b646bdbb64a4b5430fb38dc121
Ellie <elinorj@byu.edu>
2024-12-05 17:39:42 -0700

    rendering

--- src/app.jsx
+++ src/app.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
 import './app.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World! :D</h1>
+        </div>
+    );
+}
+
+export default App;
