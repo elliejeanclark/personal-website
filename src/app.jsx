7f51768bc1ae34f49c28512244f0a477340c0ccf
Ellie <elinorj@byu.edu>
2024-12-06 18:22:45 -0700

    added some packages I was missing

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './app.css';
 
 function App() {
     return (
-        <div>
-            <h1>Hello World! :D</h1>
-        </div>
+        <BrowserRouter>
+            <div>
+                <h1>Hello World! :D</h1>
+            </div>
+        </BrowserRouter>
     );
 }
 
