6a5b9f50320840696009cc9c03c700bc17f826c2
Ellie <elinorj@byu.edu>
2024-12-06 19:40:32 -0700

    routes now rendering, navbar is here.

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Home } from './Home/Home';
 import { Hobbies } from './Hobbies/Hobbies';
 import { Schooling } from './Schooling/Schooling';
 import { VolunteeringService } from './VolunteeringService/VolunteeringService';
 import { WorkExperience } from './WorkExperience/WorkExperience';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 function App() {
     return (
         <BrowserRouter>
-            <div>
-                <h1>Hello World! :D</h1>
+            <div className="body">
+                <header>
+                    <nav className="navbar navbar-expand-lg" id="head-nav">
+                        <menu className="navbar-nav">
+                            <div>
+                                Elinor Jean Clark
+                                {/* eventually a logo here hopefully */}
+                            </div>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                               <NavLink className="nav-link" to="/Hobbies">Hobbies</NavLink> 
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/Schooling">Schooling</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/VolunteeringandService">Volunteering and Service</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/WorkExperience">Work Experience</NavLink>
+                            </li>
+                        </menu>
+                    </nav>
+                </header>
             </div>
 
             <Routes>
