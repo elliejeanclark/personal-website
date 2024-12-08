8227fe4d5054777df148e33934c8fdc6ebd4fbeb
Ellie <elinorj@byu.edu>
2024-12-07 19:32:44 -0700

    started the first part of the home page.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,8 +4,14 @@ import './Home.css';
 export function Home() {
     return (
         <div>
-            <h1>Home</h1>
-            <p>Welcome to my personal website!</p>
+            <div id="card1">
+                <img src="./pictures/headshot1.jpg" alt="headshot" id="headshot1"/>
+                <p id="intro">
+                    Hello! My name is Elinor Jean Clark, and I am a computer scientist passionate about developing software that enhances people's lives. 
+                    I graduated high school at Orem High in 2021, and am currently pursuing a Bachelor's Degree in Computer Science at Brigham Young University.
+                    I currently work at the Christa McAullife Space center as a Flight Director.
+                </p>
+            </div>
         </div>
     );
 }
