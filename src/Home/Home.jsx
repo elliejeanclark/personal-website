db31f583890654b77432fda5b364c5e9f6cb49fe
Ellie <elinorj@byu.edu>
2024-12-09 18:56:52 -0700

    current iteration of the home page is done, we shall see if it sticks.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,36 +5,44 @@ export function Home() {
     return (
         <div>
             <div id="name">Elinor Jean Clark</div>
-            <div id="card1">
-                <img src="./pictures/headshot1.jpg" alt="headshot" id="headshot1"/>
-                <p id="intro">
+            <div className="cards" id="card1">
+                <img className="headshots" src="./pictures/headshot1.jpg" alt="headshot" id="headshot1"/>
+                <p className="home-text" id="intro">
                     Hello! My name is Elinor Jean Clark, and I am a computer scientist passionate about developing software that enhances people's lives. 
                     I graduated high school at Orem High in 2021, and am currently pursuing a Bachelor's Degree in Computer Science at Brigham Young University.
                     I currently work at the Christa McAullife Space center as a Flight Director.
                 </p>
             </div>
-            <div id="card2">
-                <img src="./pictures/headshot2.jpg" alt="headshot" id="headshot2" />
-                <p id="personal-info">
-                    I am the oldest daughter of 3 children, and I love my family and friends. As a member of the Church of Jesus Christ of Latter-Day Saints
-                    I have a strong faith in Jesus Christ and His teachings. I love to read, write, crochet, play the piano, complete jigsaw puzzles, attend live theater, 
-                    play board games, and watch fun movies and tv shows with my friends. I am passionate about working with others to better our community and the world. 
+            <div className="cards" id="card2">
+                <p className="home-text" id="hobbies">
+                    I am the oldest daughter of 3 children, and I love my family and friends. I love to read, write, crochet, play the piano, complete jigsaw puzzles, attend live theater, 
+                    play board games, and spend time with friends and family. I am passionate about being an active member of my community and working with others to better our community 
+                    and the world. 
                 </p>
+                <img className="headshots" src="./pictures/headshot2.jpg" alt="headshot" id="headshot2" />
             </div>
-            <div id="card3">
-                <img src="./pictures/headshot3.JPG" alt="headshot" id="headshot3" />
-                <p id="schooling">
+            <div  className="cards" id="card3">
+                <img className="headshots" src="./pictures/headshot3.JPG" alt="headshot" id="headshot3" />
+                <p className="home-text" id="schooling">
                     I graduated Orem High School in 2021, graduating with honors and the student director of the Orem High Choir. On graduating I was awarded several scholarships,
                     most notably I recieved the Daniels Fund Scholarship. I am currently pursuing a Bachelor's Degree in Computer Science at Brigham Young University, where I have
                     made the Deans list and been offered positions at the college by multiple professors.
                 </p>
             </div>
-            <div id="card4">
-                <img src="./pictures/headshot4.jpg" alt="headshot" id="headshot4" />
-                <p id="work">
+            <div className="cards" id="card4">
+                <p className="home-text" id="work">
                     I currently work at the Christa McAullife Space Center. I have been working at the center since 2021 as a Supervisor, Set Director, and 
                     currently as a Flight Director. I have worked with hundreds of children and adults, helping educate them and helping them have fun discovering the wonders of the universe
                     and their own potential. As a Flight Director, I have had a lot of experience troubleshooting and problem solving.
+                </p>
+                <img className="headshots" src="./pictures/headshot4.jpg" alt="headshot" id="headshot4" />
+            </div>
+            <div className="cards" id="card5">
+                <img className="headshots" src="./pictures/headshot5.JPG" alt="headshot" id="headshot5" />
+                <p className="home-text" id="volunteering-service">
+                    As a member of the Church of Jesus Christ of Latter-Day Saints I have a strong faith in Jesus Christ and his mission of service. I have served in several 
+                    capacities in my church. I have been a teacher, youth leader, and musical accompianist. I also served as a volunteer for 3 years at the Christa McAullife Space before 
+                    I was offered a position, and have volunteered in phone banks during elections.
                 </p>
             </div>
         </div>
