a09c89f875a43dcd379e9dadbb0b793454c114a0
Ellie <elinorj@byu.edu>
2024-12-09 18:02:50 -0700

    adding base font

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,6 +4,7 @@ import './Home.css';
 export function Home() {
     return (
         <div>
+            <p id="name">Elinor Jean Clark</p>
             <div id="card1">
                 <img src="./pictures/headshot1.jpg" alt="headshot" id="headshot1"/>
                 <p id="intro">
