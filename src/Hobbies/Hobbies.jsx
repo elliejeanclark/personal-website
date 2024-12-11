1b254a7a4aefa2f6207b9d57f1099dc6dafb7371
Ellie <elinorj@byu.edu>
2024-12-10 18:37:09 -0700

    everything is in a row

--- src/Hobbies/Hobbies.jsx
+++ src/Hobbies/Hobbies.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
+import './Hobbies.css';
 
 export function Hobbies() {
     return(
         <div>
             <div id="hobbie-selection">
-                <i class="fa-solid fa-music"></i>
-                Music
-                <i class="fa-solid fa-book-bookmark"></i>
-                reading
-                <i class="fa-solid fa-pen-clip"></i>
-                writing
-                <i class="fa-solid fa-masks-theater"></i>
-                live theater
-                <i class="fa-solid fa-person-hiking"></i>
-                walking
-                <i class="fa-regular fa-futbol"></i>
-                soccer
-                <i class="fa-solid fa-mitten"></i>
-                crochet
-                <i class="fa-solid fa-dice"></i>
-                board games
-                <i class="fa-solid fa-puzzle-piece"></i>
-                jigsaw puzzles
-                <i class="fa-solid fa-tv"></i>
-                Entertainment
-                <i class="fa-solid fa-clock"></i>
-                time with family and friends
+                <div className="hobbie-container">
+                    <i className="fa-solid fa-music"></i>
+                    <p>Music</p>
+                </div>
+                <div className="hobbie-container">
+                    <i className="fa-solid fa-book-bookmark"></i>
+                    <p>Reading</p>
+                </div>
+                <div className="hobbie-container">
+                    <i className="fa-solid fa-pen-clip"></i>
+                    <p>Writing</p>
+                </div>
+                <div className="hobbie-container">
+                    <i className="fa-solid fa-masks-theater"></i>
+                    <p>Live theater</p>
+                </div>
+                <div className="hobbie-container">
+                    <i className="fa-solid fa-person-hiking"></i>
+                    <p>Walking</p>
+                </div>
+                <div className="hobbie-container">
+                    <i className="fa-solid fa-futbol"></i>
+                    <p>Soccer</p>
+                </div>
+                <div className="hobbie-container">
+                    <i className="fa-solid fa-mitten"></i>
+                    <p>Crochet</p>
+                </div>
+                <div className="hobbie-container">
+                    <i className="fa-solid fa-dice"></i>
+                    <p>Board Games</p>
+                </div>
+                <div className="hobbie-container">
+                    <i className="fa-solid fa-puzzle-piece"></i>
+                    <p>Jigsaw Puzzles</p>
+                </div>
+                <div className="hobbie-container">
+                    <i className="fa-solid fa-tv"></i>
+                    <p>Entertainment</p>
+                </div>
             </div>
         </div>
     );
